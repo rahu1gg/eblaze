a97234f4568b2e0ea9534004a52b0f19adce8ba0
rahu1gg <rahulpalamarthi@gmail.com>
2024-02-18 08:56:36 +0530

    feat: add contact section in home route

---
+++ src/components/pages/home/contact.tsx
@@ -0,0 +1,23 @@
+import { APP } from '@/constants/app';
+import { ArrowUpRight } from 'lucide-react';
+import Link from 'next/link';
+
+export function ContactUs() {
+  return (
+    <section>
+      <div className='h-screen flex items-center justify-center'>
+        <div className='w-3/5 mx-auto'>
+          <h2 className='text-6xl/[80px] font-light'>
+            <em>LET'S TALK ABOUT EVENTS, PROJECTS AND WORK</em>
+          </h2>
+          <Link href={`mailto:${APP.email}`} className='group mt-14 h-9 inline-flex items-center justify-start gap-3'>
+            {APP.email}
+            <span className='bg-foreground inline-flex items-center justify-center text-background rounded-full size-5 overflow-hidden group-hover:size-16 duration-300'>
+              <ArrowUpRight className='-translate-x-10 translate-y-10 duration-300 group-hover:translate-x-0 group-hover:translate-y-0' size={28} />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
