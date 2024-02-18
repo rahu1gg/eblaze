ab9e4a3116ec3c483c3ca3c178ea955b56f03d62
rahu1gg <rahulpalamarthi@gmail.com>
2024-02-18 09:00:53 +0530

    enhance: shift the icons into its own file

--- src/components/global/footer/index.tsx
+++ src/components/global/footer/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { CardBody, CardContainer, CardItem } from '../../ui/3d-card';
 import { Button } from '../../ui/button';
 import { Hack } from '../hack';
+import { Arrow, Star } from '../icons';
 import { FooterLink } from './client';
 
 export function Footer() {
@@ -62,19 +63,6 @@ export function Footer() {
 }
 
 function ArrowLink() {
-  function Arrow() {
-    return (
-      <svg className='size-10 last-of-type:-translate-x-9' viewBox='0 0 99 99' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <title>Navigate</title>
-        <path
-          fillRule='evenodd'
-          clipRule='evenodd'
-          d='M0 18.084 16.925 0H99v81.38L80.28 99V58.698c0-9.81.097-18.696.3-26.668L14.829 97.163 1.799 82.909l65.75-65.13q-11.683.306-26.21.305z'
-          fill='currentColor'
-        />
-      </svg>
-    );
-  }
   return (
     <div className='relative ml-1 size-10 overflow-hidden'>
       <div className='absolute transition-all duration-200 group-hover:-translate-y-10 group-hover:translate-x-9'>
@@ -82,17 +70,5 @@ function ArrowLink() {
         <Arrow />
       </div>
     </div>
-  );
-}
-
-function Star() {
-  return (
-    <svg width='31' height='30' className='size-9' viewBox='0 0 31 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <title>Navigate</title>
-      <path
-        d='m15.272 22.192-5.688 7.632-5.688-4.464L9.8 17.44.296 14.416l2.376-6.768 9 2.952V.736h7.272V10.6l9-2.952 2.376 6.768-9.504 3.024 5.904 7.92-5.688 4.464z'
-        fill='#000'
-      />
-    </svg>
   );
 }
