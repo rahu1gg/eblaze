9e21dffdabd3961efffe2edfa791f92836af499e
rahu1gg <rahulpalamarthi@gmail.com>
2024-02-18 10:40:37 +0530

    styles: change the icon in the contact section in home route

--- src/components/pages/home/contact.tsx
+++ src/components/pages/home/contact.tsx
@@ -1,10 +1,10 @@
+import { Arrow } from '@/components/global/icons';
 import { APP } from '@/constants/app';
-import { ArrowUpRight } from 'lucide-react';
 import Link from 'next/link';
 
 export function ContactUs() {
   return (
-    <section>
+    <section className='bg-muted'>
       <div className='h-screen flex items-center justify-center'>
         <div className='w-3/5 mx-auto'>
           <h2 className='text-6xl/[80px] font-light'>
@@ -13,7 +13,7 @@ export function ContactUs() {
           <Link href={`mailto:${APP.email}`} className='group mt-14 h-9 inline-flex items-center justify-start gap-3'>
             {APP.email}
             <span className='bg-foreground inline-flex items-center justify-center text-background rounded-full size-5 overflow-hidden group-hover:size-16 duration-300'>
-              <ArrowUpRight className='-translate-x-10 translate-y-10 duration-300 group-hover:translate-x-0 group-hover:translate-y-0' size={28} />
+              <Arrow className='-translate-x-10 translate-y-10 duration-300 group-hover:translate-x-0 group-hover:translate-y-0 size-4' />
             </span>
           </Link>
         </div>
