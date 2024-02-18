dc00d8706ebd385a299be3c5aa141be94222d984
rahu1gg <rahulpalamarthi@gmail.com>
2024-02-18 10:51:40 +0530

    feat: add footer to home route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Footer } from '@/components/global/footer';
+import { ContactUs } from '@/components/pages/home/contact';
+import { Fragment } from 'react';
+
 export default function Home() {
   return (
-    <main>
-      <div>
-        <p>Hello world</p>
-      </div>
-    </main>
+    <Fragment>
+      <main>
+        <div className='h-[300vh]'>
+          <p>Hello world</p>
+        </div>
+        <ContactUs />
+      </main>
+      <Footer />
+    </Fragment>
   );
 }
